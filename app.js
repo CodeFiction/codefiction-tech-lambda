18d32c5b7d85a9cf7768593261db52005fba3b5d
msusur <mail@mertsusur.com>
2017-07-11 23:38:30 +0100

    Adds get episodes

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    app = express(),
+    parser = require('rss-parser'),
+    rssUrl = 'http://feeds.soundcloud.com/users/soundcloud:users:264614350/sounds.rss';
 
-app.get('/', function(req, res) {
-  res.send({
-    "Output": "Hello World!"
-  });
+app.get('/episodes', function(req, res) {
+    parser.parseURL(rssUrl, function(err, feed) {
+        var items = [],
+            list = feed.rss.channel;
+        for (var key in list) {
+            if (key === 'item') {
+                items.push({
+                    title: list[key].title,
+                    url: list[key].url,
+                    duration: list[key]['itunes:duration'],
+                    date: list[key].pubDate,
+                    description: list[key].description,
+                });
+            }
+        }
+        res.send(items);
+    });
 });
-
-app.post('/', function(req, res) {
-  res.send({
-    "Output": "Hello World!"
-  });
-});
-
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
-module.exports = app
+module.exports = app;
