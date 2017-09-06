55b7170aacf09cdff6300501f064efeab66208a5
Onur Aykac <onuraykac@gmail.com>
2017-09-06 22:43:22 +0800

    added search api and refactored root api route

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const express = require('express'),
     parser = require('rss-parser'),
     rssUrl = 'http://feeds.soundcloud.com/users/soundcloud:users:264614350/sounds.rss';
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-app.get('/', function(req, res) {
-    parser.parseURL(rssUrl, function(err, rss) {
+app.get('/all', function (req, res) {
+    parser.parseURL(rssUrl, function (err, rss) {
         var items = [],
             list = rss.feed.entries;
         for (var i = 0; i < list.length; i++) {
@@ -23,6 +23,29 @@ app.get('/', function(req, res) {
                 description: list[i].content,
             });
         }
+        res.json(items);
+    });
+});
+
+app.get('/search', function (req, res) {
+    var keyword = req.query.keyword;
+    parser.parseURL(rssUrl, function (err, rss) {
+        var items = [],
+            list = rss.feed.entries;
+        for (var i = 0; i < list.length; i++) {
+            if (list[i].content.toLowerCase().includes(keyword.toLowerCase())
+                || list[i].title.toLowerCase().includes(keyword.toLowerCase())) {
+                items.push({
+                    title: list[i].title,
+                    url: list[i].link,
+                    duration: list[i].itunes.duration,
+                    image: list[i].itunes.image,
+                    date: list[i].pubDate,
+                    description: list[i].content,
+                });
+            }
+        }
+
         res.json(items);
     });
 });
